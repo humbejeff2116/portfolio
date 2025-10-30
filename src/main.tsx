a92ae08ef9933525b05d4fd9f5a09698df02bafc
humbejeff2116 <humbejeff2116@gmail.com>
2025-10-30 08:50:18 +0100

    attach Theme and Helmet context providers and wrap App inside Browser router Component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { HelmetProvider } from 'react-helmet-async'
+import { ThemeProvider } from './context/theme/Theme.contextProvider.tsx'
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+    <ThemeProvider>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+    </ThemeProvider>
+    </Router>
   </StrictMode>,
 )
