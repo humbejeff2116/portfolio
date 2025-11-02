3d43536dbffd4bb762e4a41741571e34bbb930c2
humbejeff2116 <humbejeff2116@gmail.com>
2025-11-02 09:46:43 +0100

    implement BaseContainer component

---
+++ src/components/BaseAppContainer.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+export default function BaseAppContainer({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="transition-colors duration-700 ease-in-out bg-background-light dark:bg-background-dark text-zinc-900 dark:text-zinc-100 min-h-screen">
+            {children}
+        </div>
+    )
+}
