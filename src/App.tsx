0810074e43db6d689e213871a7baa959bca5d3c3
humbejeff2116 <humbejeff2116@gmail.com>
2025-11-02 09:40:33 +0100

    add not found route and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import './App.css';
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-import SmoothScroll from "./components/SmoothScroll";
 import Navbar from "./components/Navbar";
 import { ThemeTransitionOverlay } from "./components/ThemeTransitionOverlay";
 import { CustomCursor } from "./components/CustomCursor";
-// import Footer from "./components/Footer";
 import { AnimatePresence, motion } from "framer-motion";
 // import PageTransitionOverlay from "./components/PageTransitionOverlay";
 import ScrollProgressBar from "./components/ScrollProgressBar";
 import DynamicLight from "./components/DynamicLight";
 import { useSmoothScroll } from "./hooks/useSmoothScroll";
-// import { useSmoothScroll } from "./hooks/useSmoothScroll";
+import { SmoothScroll } from "./components/SmoothScroll";
+import { ScrollToTopButton } from "./components/ScrollToTopButton";
+import './App.css';
+import NotFound from "./pages/NotFound";
 
 function App() {
   const location = useLocation();
@@ -21,7 +21,6 @@ function App() {
   return (
     <div className="relative bg-black text-white min-h-screen overflow-x-hidden">
     <SmoothScroll>
-      
       <Navbar />
       <ScrollProgressBar />
       {/* <PageTransitionOverlay /> */}
@@ -53,15 +52,29 @@ function App() {
               </motion.div>
             }
           />
+
+          <Route
+            path="*"
+            element={
+              <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -30 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+              >
+                <NotFound />
+              </motion.div>
+            }
+          />
         </Routes>
       </AnimatePresence>
-      {/* Cinematic transition effect */}
       <ThemeTransitionOverlay />
       <CustomCursor />
-      {/* <Footer/> */}
+        {/* Not yet visible TODO... fix it */}
+      <DynamicLight />
+      <ScrollToTopButton />
     </SmoothScroll>
-    {/* Not yet visible TODO... fix it */}
-    <DynamicLight />
+ 
     </div>
   );
 }
