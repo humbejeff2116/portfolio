54dfcb76da1ec9bde586e3b00a569a1d33221364
humbejeff2116 <humbejeff2116@gmail.com>
2025-11-12 13:05:19 +0100

    implement an Image component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { useParallaxReveal } from "../hooks/useParallaxReveal";
 import FloatingParticles from "./FloatingParticles";
+import aboutMe from '../assets/jeffweb3.jpg';
 
 const skills = ["TypeScript", "Golang", "React", "Next.js", "Node.js"];
 
@@ -18,8 +19,9 @@ export default function About() {
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
-            className="bg-gradient-to-b from-gray-950 to-black md:bg-white/0.5 backdrop-blur-lg md:backdrop-blur-sm z-20 max-w-3xl px-5 py-10 rounded-2xl shadow-sm shadow-indigo-500"
+            className="overflow-hidden relative bg-gradient-to-b from-gray-950 to-black md:bg-white/0.5 backdrop-blur-lg md:backdrop-blur-sm z-20 max-w-3xl px-5 py-10 rounded-2xl shadow-sm shadow-indigo-500"
         >
+            
             <motion.h2
                 className="text-4xl md:text-5xl font-bold text-center py-2 bg-gradient-to-t from-sky-500 to-indigo-600 bg-clip-text text-transparent mb-16"
                 {...headingText}
@@ -74,6 +76,20 @@ function Skill({
             className="px-6 py-2 text-gray-400 font-medium rounded-full bg-white/5 border border-white/10 hover:border-indigo-400 hover:text-indigo-400 transition-all duration-300"
         >
             <p>{skill}</p>
+        </motion.div>
+    )
+}
+
+
+export function MyImage() {
+    return (
+        <motion.div 
+            initial={{ scale: 0.8 }}
+            whileInView={{ scale: 1 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="z-[0] bg-gray-900 cursor-pointer transition-all duration-300 flex items-center justify-center absolute bottom-[-10rem] left-[-10rem] shadow-sm shadow-indigo-900 "
+        >
+            <img src={aboutMe} className="w-[100%] h-[100%]" alt="Humbe Jeffrey"/>
         </motion.div>
     )
 }
